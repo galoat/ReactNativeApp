830aab6afa9ca90dceae0de334cd00507f8a6e3b
flacour <flacour@partner.auchan.fr>
2018-04-18 14:02:11 +0200

    add Personne vlient server

---
+++ server/personne-client/src/main/java/com/skiut/personneclient/PersonneClientApplication.java
@@ -0,0 +1,16 @@
+package com.skiut.personneclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PersonneClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonneClientApplication.class, args);
+	}
+}
