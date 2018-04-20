1d302f4f71d1a6774e5a13bbb2ed40c9df3dd106
flacour <flacour@partner.auchan.fr>
2018-04-20 13:46:25 +0200

    add eurecka service to personne service

--- server/personne-service/src/main/java/com/skiut/personneservice/PersonneServiceApplication.java
+++ server/personne-service/src/main/java/com/skiut/personneservice/PersonneServiceApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
