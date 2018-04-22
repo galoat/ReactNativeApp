4f0d548f8e50d477169c3891d59554d49c5984f4
galoat <florianlacour@hotmail.fr>
2018-04-22 15:21:36 +0200

    hystrix dashboard working

--- server/personne-client/src/main/java/com/skiut/personneclient/restController/feignClient/FeignPersonneService.java
+++ server/personne-client/src/main/java/com/skiut/personneclient/restController/feignClient/FeignPersonneService.java
@@ -2,7 +2,6 @@ package com.skiut.personneclient.restController.feignClient;
 
 import com.skiut.personneclient.entity.Personne;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
