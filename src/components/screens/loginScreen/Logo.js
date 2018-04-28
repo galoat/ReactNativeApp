42e898e828cc966f5025e2e3079f2ebacd8609e3
galoat <florianlacour@hotmail.fr>
2018-04-28 19:27:22 +0200

    logo get the good size

--- src/components/screens/loginScreen/Logo.js
+++ src/components/screens/loginScreen/Logo.js
@@ -1,25 +1,35 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Dimensions from 'Dimensions';
-import {StyleSheet, Image} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 
 import bgSrc from '../../../img/logo.png';
 
 export default class Wallpaper extends Component {
   render() {
     return (
+      <View style={styles.container}>
       <Image style={styles.picture} source={bgSrc}>
         {this.props.children}
       </Image>
+    </View>
     );
   }
 }
+const DEVICE_WIDTH = Dimensions.get('window').width;
+const DEVICE_HEIGHT = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-  picture: {
+  container: {
     flex: 1,
-    width: null,
-    height: null,
-    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  picture: {
+    flex: 0.8,
+    height: DEVICE_HEIGHT,
+    width:DEVICE_WIDTH / 2,
+    resizeMode: 'contain',
   },
 });
