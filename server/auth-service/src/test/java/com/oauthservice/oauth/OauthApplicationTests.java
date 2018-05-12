e1560249e96e7f5802870ab6a91d460303ab8271
galoat <florianlacour@hotmail.fr>
2018-05-12 19:14:49 +0200

    add d2 fo test oauth

--- server/auth-service/src/test/java/com/oauthservice/oauth/OauthApplicationTests.java
+++ server/auth-service/src/test/java/com/oauthservice/oauth/OauthApplicationTests.java
@@ -2,11 +2,16 @@ package com.oauthservice.oauth;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class OauthApplicationTests {
 
 	@Test
