85bc6d8ba0c25a20efe7d0b47087c78ec58a9f32
flacour <flacour@partner.auchan.fr>
2018-05-14 13:40:23 +0200

    add Test

--- server/auth-service/src/test/java/com/oauthservice/oauth/OauthApplicationTests.java
+++ server/auth-service/src/test/java/com/oauthservice/oauth/OauthApplicationTests.java
@@ -1,21 +1,77 @@
 package com.oauthservice.oauth;
 
-import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
+
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@WebAppConfiguration
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class OauthApplicationTests {
+	@Autowired
+	private AccountRepository accountRepository;
 
-	@Test
-	public void contextLoads() {
+
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mockMvc;
+
+	private static final String CLIENT_ID = "html5";
+	private static final String CLIENT_SECRET = "secret";
+
+	private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+	@Before
+	public void autoAdd(){
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
+		Stream.of("jlong,spring","test,test","test2,test2" ).map(t->t.split(",")).
+						forEach(t->this.accountRepository.save(new Account(t[0],t[1],true)););
 	}
+	private String obtainAccessToken(String username, String password) throws Exception {
+		final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		params.add("grant_type", "password");
+		params.add("client_id", CLIENT_ID);
+		params.add("username", username);
+		params.add("password", password);
 
+		// @formatter:off
+
+        ResultActions result = mockMvc.perform(post("/uaa/oauth/token")
+                               .params(params)
+                               .with(httpBasic(CLIENT_ID, CLIENT_SECRET))
+                               .accept(CONTENT_TYPE))
+                               .andExpect(status().isOk())
+                               .andExpect(content().contentType(CONTENT_TYPE));
+
+        // @formatter:on
+
+		String resultString = result.andReturn().getResponse().getContentAsString();
+
+		JacksonJsonParser jsonParser = new JacksonJsonParser();
+		return jsonParser.parseMap(resultString).get("access_token").toString();
+	}
 }
