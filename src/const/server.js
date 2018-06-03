90b7d33e1d3b1e80cb1ba931298b2dfce9eee5ea
galoat <florianlacour@hotmail.fr>
2018-06-03 16:57:25 +0200

    Connection working

--- src/const/server.js
+++ src/const/server.js
@@ -3,3 +3,4 @@ export const SERVER_PORT_OAUTH = '9191';
 export const SERVER_OAUTH_PATH = '/uaa/oauth/token';
 export const SERVER_OAUTH_ID  = 'html5';
 export const SERVER_OAUTH_SECRET  = 'secret';
+export const SERVER_BASE64  = 'aHRtbDU6c2VjcmV0';
