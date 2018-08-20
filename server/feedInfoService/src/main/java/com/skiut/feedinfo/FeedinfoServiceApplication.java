3907e1014ad7f71f140d72ac7181d913825ebc7c
galoat <florianlacour@hotmail.fr>
2018-08-20 21:58:52 +0200

    add feedINfoService

---
+++ server/feedInfoService/src/main/java/com/skiut/feedinfo/FeedinfoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.skiut.feedinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedinfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedinfoServiceApplication.class, args);
+	}
+}
--- server/personne-client/src/main/java/com/skiut/personneclient/PersonneClientApplication.java
+++ server/personne-client/src/main/java/com/skiut/personneclient/PersonneClientApplication.java
@@ -2,21 +2,15 @@ package com.skiut.personneclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.IntegrationComponentScan;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.NegativeOrZero;
+
 
 
 @EnableResourceServer
