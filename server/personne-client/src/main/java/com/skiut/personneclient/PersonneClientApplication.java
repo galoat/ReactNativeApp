08758d582a50028f4dee27df517f85c75d39d6aa
galoat <florianlacour@hotmail.fr>
2018-08-24 23:11:28 +0200

    FeedInfo added

--- server/personne-client/src/main/java/com/skiut/personneclient/PersonneClientApplication.java
+++ server/personne-client/src/main/java/com/skiut/personneclient/PersonneClientApplication.java
@@ -3,17 +3,16 @@ package com.skiut.personneclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 
-
-
-@EnableResourceServer
+@EnableCircuitBreaker
 @EnableZuulProxy
 @EnableFeignClients
 @EnableDiscoveryClient
