03fa9bcbd7544db716a6f2024afd0c0c28b8f362
galoat <florianlacour@hotmail.fr>
2018-09-08 12:14:23 +0200

    get rich text content on click

--- server/webApp/src/main/java/com/skiut/webapp/controller/MainPageController.java
+++ server/webApp/src/main/java/com/skiut/webapp/controller/MainPageController.java
@@ -1,16 +1,31 @@
 package com.skiut.webapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 @Controller
 public class MainPageController
 {
+
+	private Logger logger = LoggerFactory.getLogger(MainPageController.class);
+
 	@RequestMapping("/mainPage")
 	String mainPage(Model model,HttpSession session ) {
+
+
+		model.addAttribute("text", new String());
 		return "mainPage"; // src/main/ressources/template/+$x+.html
 	}
 
+
+	@RequestMapping("/sendrichtext")
+	String sendRichText(@ModelAttribute("text") String text){
+		logger.info("Rich String to send "+ text);
+		return "mainPage";
+	}
 }
