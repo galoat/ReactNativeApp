b6b63d9fd49c63947b7591c1cc86a5a9b2da7097
galoat <florianlacour@hotmail.fr>
2018-09-09 16:57:57 +0200

    respond json

--- server/personne-client/src/main/java/com/skiut/personneclient/restController/controller/FeedApiAdapterRestController.java
+++ server/personne-client/src/main/java/com/skiut/personneclient/restController/controller/FeedApiAdapterRestController.java
@@ -5,7 +5,13 @@ import com.skiut.personneclient.entity.Feed;
 import com.skiut.personneclient.restController.controller.feignClient.FeignFeedService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.experimental.var;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @AllArgsConstructor
@@ -30,19 +38,28 @@ public class FeedApiAdapterRestController {
     @Autowired
     private FeignFeedService feignFeedService;
 
-    public Collection<String> fallbackFeed(){return new ArrayList<>();}
+    public String fallbackFeed(){return StringUtils.EMPTY;}
 
     @HystrixCommand(fallbackMethod = "fallbackFeed")
     @GetMapping("/getAll")
-    public Collection<String> allFeeds(){
+    public String allFeeds(){
         log.info("Content : "+ getFeignFeedService().getFeeds()
                 .getContent());
 
-        return getFeignFeedService().getFeeds()
-                .getContent()
-                .stream()
-                .map(Feed::getFeed)
-                .collect(Collectors.toList());
+
+
+        JSONArray jsonArray = new JSONArray();
+        for(Feed a :  getFeignFeedService().getFeeds().getContent()){
+            JSONObject j1 = new JSONObject();
+            try {
+                j1.put ("feed", a.getFeed());
+                jsonArray.put(j1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return jsonArray.toString();
+
     }
 
 }
