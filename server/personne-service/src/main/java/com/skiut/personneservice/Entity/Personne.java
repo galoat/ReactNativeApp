2b5eca3dde9bf1eb9e25a3c7f83276557d23bf3b
florian lacour <flacour@partner.auchan.com>
2018-10-29 13:48:40 +0100

    Server : JAVA10 migration App finish

--- server/personne-service/src/main/java/com/skiut/personneservice/Entity/Personne.java
+++ server/personne-service/src/main/java/com/skiut/personneservice/Entity/Personne.java
@@ -8,17 +8,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+/**
+ * The type Personne.
+ */
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Personne
 {
+	/**
+	 * The Id.
+	 */
 	@Id
 	@GeneratedValue
 	private Long id;
+	/**
+	 * The Name.
+	 */
 	private String name;
 
+	/**
+	 * Instantiates a new Personne.
+	 *
+	 * @param name the name
+	 */
 	public Personne(String name)
 	{
 		this.name = name;
